7ec936c6ccc7ce1f57861b0c05b9e28c08b4e4a1
Kevin Jose Quiñones Baldion <73363421+baldion7@users.noreply.github.com>
2023-07-29 11:12:22 -0500

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -4,7 +4,7 @@ import argon2 from "argon2";
 export const Login = async (req, res) => {
     const { usuario ,password} = req.body;
     if (!usuario || !password) {
-        return res.status(400).json({ msg: "Ingrese un correo electrónico y una contraseña" });
+        return  res.redirect("/");
     }
 
     const user = await User.findOne({
@@ -16,9 +16,9 @@ export const Login = async (req, res) => {
         },
     });
 
-    if (!user) return res.status(404).json({ msg: "Usuario no encontrado" });
+    if (!user) return res.redirect("/");
     const match = await argon2.verify(user.Password, password);
-    if (!match) return res.status(400).json({ msg: "Contraseñas incorrectas" });
+    if (!match) return  res.redirect("/");
     req.session.userId = user.Id;
     req.session.rolName = user.role.Name;
     const rol = user.role.Name;
@@ -26,19 +26,19 @@ export const Login = async (req, res) => {
 };
 export const Me = async (req,res)=>{
     if (!req.session.userId){
-        return res.status(401).json({msg:"Por favor, ingrese a su cuenta"});
+        return  res.redirect("/");
     }
     const user= await User.findOne({
         where:{
             id: req.session.userId
         }
     });
-    if (!user) return res.status(404).json({msg:"Usuario no encontrado"});
+    if (!user) return  res.redirect("/");
     res.status(200).json(user);
 };
 export const logOut = (req, res)=>{
     req.session.destroy((err)=>{
-        if(err) return res.status(400).json({msg: "No puedo cerrar sesión"})
+        if(err) return  res.redirect("/");
         res.redirect("/");
     })
 }
