c9af566ce5ec55443ab736d5b5b93336bf5a86cd
baldi <baldionkevin8@gmail.com>
2023-08-14 16:26:01 -0500

    Actualizacion de controlador

--- routes/ViewRouter.js
+++ routes/ViewRouter.js
@@ -3,8 +3,8 @@ import {GarmentView,AllGarmentView,LoginView} from "../controllers/View.js";
 import {verifyUser, adminOnly,AtchUser} from "../middleware/AuthUser.js";
 
 const router = express.Router();
-router.get('/Germents/:id',verifyUser,GarmentView);
-router.get('/Germent',verifyUser,AllGarmentView);
-router.get ('/',AtchUser,LoginView)
+router.get('/Germents/:id',GarmentView);
+router.get('/Germent',AllGarmentView);
+router.get ('/',LoginView)
 
 export default router;
