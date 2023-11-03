304b43e9c2868281ea94e51552c641b1ea91c6a0
baldi <baldionkevin8@gmail.com>
2023-11-02 21:08:20 -0500

    terminacion de controladores y rutas

--- controllers/ImgDetails.js
+++ controllers/ImgDetails.js
@@ -1,10 +1,10 @@
 import {ImgDetails} from "../models/ImgDetailsModel.js";
-import { ImgGarment } from '../models/ImgGarmentModel.js'
+
 
 export const CreateImgDetails = async (req, res) => {
     const {name,imgroute,btndetailId} = req.body;
     try {
-        const respuesta = await ImgGarment.create({
+        const respuesta = await ImgDetails.create({
             Name: name, img_route: imgroute, btndetailId:btndetailId
         });
         res.status(200).json(respuesta);
