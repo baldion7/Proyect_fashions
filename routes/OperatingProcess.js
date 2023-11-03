e67177c9174381da509cb3cf9bf0ec3adce46209
baldi <baldionkevin8@gmail.com>
2023-11-03 17:16:12 -0500

    terminacion de controladores y rutas

--- routes/OperatingProcess.js
+++ routes/OperatingProcess.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { CreateOperatingProcess, DeleteOperatingProcess, GetOperatingProcess, GetOperatingProcessById, UpdateOperatingProcess } from '../controllers/OperatingProcess.js'
 const router = express.Router();
-router.get('/api/allowarmedInfo',GetOperatingProcess);
-router.get('/api/allowarmedInfo/:id',GetOperatingProcessById);
-router.post('/api/allowarmedInfo',CreateOperatingProcess);
-router.delete('/api/allowarmedInfo',DeleteOperatingProcess);
-router.patch('/api/allowarmedInfo/:id',UpdateOperatingProcess);
+router.get('/api/operatingprocess',GetOperatingProcess);
+router.get('/api/operatingprocess/:id',GetOperatingProcessById);
+router.post('/api/operatingprocess',CreateOperatingProcess);
+router.delete('/api/operatingprocess',DeleteOperatingProcess);
+router.patch('/api/operatingprocess/:id',UpdateOperatingProcess);
 export default router;
